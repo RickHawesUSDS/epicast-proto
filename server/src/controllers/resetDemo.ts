95621ab71f2cb0f87260782a00fc5d074362090e
Rick Hawes <rhawes@cdc.gov>
2022-10-02 20:25:13 +0100

    Reset deletes state cases

--- server/src/controllers/resetDemo.ts
+++ server/src/controllers/resetDemo.ts
@@ -1,8 +1,12 @@
 import { db } from "../utils/db"
+import { StateCase } from "@/models/StateCase"
 import { insertFakeStateCases } from "../services/fakeStateCase"
 
 async function resetDemo() {
     await db.sync()
+    await StateCase.destroy({
+        truncate: true
+    })
     await insertFakeStateCases(new Date(), 3)
     console.log("Reset Demo")
 }
