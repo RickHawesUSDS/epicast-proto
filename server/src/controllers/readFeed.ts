060b1a83d75d3e15cc649d55b9ae0dc70e19a0d3
Rick Hawes <rhawes@cdc.gov>
2022-10-30 06:30:17 -0700

    make it a function refactoring

--- server/src/controllers/readFeed.ts
+++ server/src/controllers/readFeed.ts
@@ -5,6 +5,13 @@ import { readTimeSeries } from './readTimeSeries'
 import { VERSION_MARKER } from '@/models/feedBucketKeys'
 
 export async function readFeed<T>(fromBucket: FeedBucket, timeSeries: MutableTimeSeries<T>): Promise<Date | undefined> {
+  return readConsistently(fromBucket, timeSeries, async () => {
+    await readSchema(fromBucket, timeSeries)
+    return await readTimeSeries(fromBucket, timeSeries)
+  })
+}
+
+async function readConsistently<T>(fromBucket: FeedBucket, timeSeries: MutableTimeSeries<T>, action: () => Promise<Date | undefined>) {
   const versionMarkerExists = await fromBucket.doesObjectExist(VERSION_MARKER)
   if (!versionMarkerExists) return undefined
 
@@ -12,11 +19,12 @@ export async function readFeed<T>(fromBucket: FeedBucket, timeSeries: MutableTim
   const versionMarkerBefore = JSON.parse(versionMarkerBeforeRaw)
   if (versionMarkerBefore.updating as boolean) return undefined // don't read an updating feed
 
-  await readSchema(fromBucket, timeSeries)
-  const lastUpdate = await readTimeSeries(fromBucket, timeSeries)
+  // do it
+  const lastUpdateAt = await action()
 
   const versionMarkerAfterRaw = await fromBucket.getObject(VERSION_MARKER)
   const versionMarkerAfter = JSON.parse(versionMarkerAfterRaw)
   if (versionMarkerAfter.updating as boolean && versionMarkerAfter.version !== versionMarkerBefore.version) return undefined
-  return lastUpdate
+
+  return lastUpdateAt
 }
