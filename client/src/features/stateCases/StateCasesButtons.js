0da554402e58d53ad5b8ee56689088486bc6f22c
Rick Hawes <rhawes@cdc.gov>
2022-11-07 11:47:46 -0800

    Add a dummy change schema dialog

--- client/src/features/stateCases/StateCasesButtons.js
+++ client/src/features/stateCases/StateCasesButtons.js
@@ -1,5 +1,6 @@
+import React from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { Button, ButtonGroup, makeStyles } from '@material-ui/core'
+import { Button, ButtonGroup, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField, makeStyles } from '@material-ui/core'
 
 import { stateCases } from './stateCasesKeys'
 import { addRandomStateCases, publishStateCases  } from '../api/api'
@@ -7,6 +8,15 @@ import { addRandomStateCases, publishStateCases  } from '../api/api'
 
 export default function StateCasesButtons(props) {
   const queryClient = useQueryClient()
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
 
   const addCasesMutation = useMutation({
     mutationFn: async (params) => { return await addRandomStateCases(params.numOfDays, params.numPerDay) },
@@ -17,8 +27,8 @@ export default function StateCasesButtons(props) {
     mutationFn: async () => { return await publishStateCases() }
   })
 
-  async function onChangeSchemaClick() {
-    console.log('change schema click')
+  function onChangeSchemaClick() {
+    handleClickOpen()
   }
 
   const useButtonStyles = makeStyles((theme) => ({
@@ -40,6 +50,31 @@ export default function StateCasesButtons(props) {
       </ButtonGroup>
       <Button onClick={() => onChangeSchemaClick()} color='primary' variant='outlined'>Change Data Dictionary</Button>
       <Button disabled={publishStateCasesMutation.isLoading} onClick={() => publishStateCasesMutation.mutate()} color='primary' variant='outlined'>Publish</Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
