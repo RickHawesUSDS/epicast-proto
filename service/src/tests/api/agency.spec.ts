fd306708f7b76617dab29c106e7bda739680c226
Rick Hawes <rhawes@cdc.gov>
2022-12-18 15:50:33 -0800

    Improved API tests

--- service/src/tests/api/agency.spec.ts
+++ service/src/tests/api/agency.spec.ts
@@ -3,7 +3,7 @@ import { spec } from 'pactum'
 import { app } from '../../server/app'
 
 const caAgency = 'cphd.ca.gov'
-const baseUrl = 'http://localhost:4001/api/agencies'
+const baseUrl = 'http://localhost:4001/api'
 
 beforeAll(async () => {
   await app.start()
@@ -20,10 +20,38 @@ afterAll(async () => {
 })
 
 describe('agency tests', () => {
-  test('add random test', async () => {
+  test('add random case test', async () => {
     await spec()
-      .post(`${baseUrl}/${caAgency}/random`)
+      .post(`${baseUrl}/agencies/${caAgency}/random`)
       .expectStatus(200)
+
+    await spec()
+      .get(`${baseUrl}/agencies/${caAgency}`)
+      .expectStatus(200)
+      .expectJsonLength(1)
+
+  })
+
+  test('publish a random case test', async () => {
+    await spec()
+      .post(`${baseUrl}/agencies/${caAgency}/random`)
+      .expectStatus(200)
+
+    await spec()
+      .get(`${baseUrl}/agencies/${caAgency}`)
+      .expectStatus(200)
+      .expectJsonLength(1)
+
+    await spec()
+      .post(`${baseUrl}/agencies/${caAgency}/publish`)
+      .expectStatus(200)
+
+    await spec()
+      .get(`${baseUrl}/feeds/content?file=${caAgency}/cases/summary.yaml`)
+      .expectStatus(200)
+      .expectBodyContains('eventCount: 1')
+      .expectBodyContains('topicId: cases')
+      .expectBodyContains(`reporterId: ${caAgency}`)
   })
 })
 
