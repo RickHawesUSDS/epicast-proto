765d8aeab922cfa9e1bc71c2551f667cab372dc8
Rick Hawes <rhawes@cdc.gov>
2022-12-21 14:54:10 -0800

    Add USWDS stuff

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,16 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import { Header, Menu, NavDropDownButton, NavMenuButton, PrimaryNav, Title } from '@trussworks/react-uswds'
+
+
+const onToggle = (
+  index: number,
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean[]>>
+): void => {
+  setIsOpen((prevIsOpen) => {
+    const newIsOpen = [false, false]
+    newIsOpen[index] = !prevIsOpen[index]
+    return newIsOpen
+  })
+}
+
 
 function App() {
+  const [expanded, setExpanded] = useState(false)
+  const onClick = (): void => setExpanded((prvExpanded) => !prvExpanded)
+
+  const sectionItems = [
+    <a href="#linkOne" key="one">
+      Section 1
+    </a>,
+    <a href="#linkTwo" key="two">
+      Section 2
+    </a>,
+  ]
+
+  const [isOpen, setIsOpen] = useState([false, false])
+
+  const navMenu = [
+    <>
+      <NavDropDownButton
+        menuId="testDropDownOne"
+        onToggle={(): void => {
+          onToggle(0, setIsOpen)
+        }}
+        isOpen={isOpen[0]}
+        label="Sections"
+        isCurrent={true}
+      />
+      <Menu
+        key="one"
+        items={sectionItems}
+        isOpen={isOpen[0]}
+        id="testDropDownOne"
+      />
+    </>,
+    <a href="#three" key="three" className="usa-nav__link">
+      <span>About</span>
+    </a>,
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>EpiCast</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      <Header basic={true}>
+        <div className="usa-nav-container">
+          <div className="usa-navbar">
+            <Title>EpiCast Feed</Title>
+            <NavMenuButton onClick={onClick} label="Menu" />
+          </div>
+          <PrimaryNav
+            items={navMenu}
+            mobileExpanded={expanded}
+            onToggleMobileNav={onClick}>
+          </PrimaryNav>
+        </div>
+      </Header>
     </div>
   );
 }
